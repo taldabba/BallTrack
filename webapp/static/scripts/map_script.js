2d13868b314cde091e6136584f103f40df34ec6b
Taim <taldabba@gmail.com>
2022-01-08 11:58:58 -0500

    Custom map marker

--- webapp/static/scripts/map_script.js
+++ webapp/static/scripts/map_script.js
@@ -5,6 +5,29 @@ function initMap() {
         {
             center: {lat: 43.25472411693157, lng: -79.85625161100032},
             zoom: 8,
-            mapId: '6a6625a538e03514' 
+            mapId: '6a6625a538e03514',
+            disableDefaultUI: true
         });
+    
+    const parkIcon = "../images/balltrack_mapmarker.png";
+
+    const icons = { 
+        park: {
+            icon: parkIcon
+        }
+    };
+
+    const features = [
+        {
+            position: new google.maps.LatLng(43.22001133661375, -79.94849177318447),
+            type: "park"
+        }];
+
+    for (let i = 0; i < features.length; i++) {
+        const marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+        });
+    }
 }
