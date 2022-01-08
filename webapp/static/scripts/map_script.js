809ab5ee9159607564747d77bfc06246e883c874
Taim <taldabba@gmail.com>
2022-01-07 23:19:04 -0500

    Add custom map theme

--- webapp/static/scripts/map_script.js
+++ webapp/static/scripts/map_script.js
@@ -3,7 +3,8 @@ function initMap() {
     map = new google.maps.Map(
         document.getElementById("map"), 
         {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
+            center: {lat: 43.25472411693157, lng: -79.85625161100032},
+            zoom: 8,
+            mapId: '6a6625a538e03514' 
         });
 }
