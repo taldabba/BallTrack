362c0c758cf1176c6e1add3a1d7eea2bbc3c3aea
hamza7223 <xxh42069xx@gmail.com>
2022-01-09 02:23:51 -0500

    Updated new pfp section

--- webapp/models.py
+++ webapp/models.py
@@ -1,26 +1,31 @@
-from . import db
+from datetime import datetime
+from flaskblog import db, login_manager
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class User(db.Model, UserMixin):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    image_file = db.Column(db.String(150), nullable=False, default='d.jpg')
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-
-    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
