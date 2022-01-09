c9911a0362a3dec3f7fe4605b1cdbabb93a29699
Taim Al-Dabbagh <79340829+taldabba@users.noreply.github.com>
2022-01-09 01:44:38 -0500

    Revert "Integrate map court view"

--- webapp/views.py
+++ webapp/views.py
@@ -51,8 +51,7 @@ def see_court():
         courtName = request.args.get('name')
         
 
-        # if courts.find({'name':courtName}).count() == 0:
-        if 1 == 0:
+        if courts.find({'name':courtName}).count() == 0:
             return redirect("/show-courts", code=302)
         else:
             court = courts.find_one({'name':courtName})
@@ -73,14 +72,6 @@ def see_court():
         db.session.commit()
         
         return redirect(f"/courts/?name={courtName}", code=302)
-
-    else:
-         return redirect("/show-courts", code=302)
-
-@views.route("/register-court")
-@login_required
-def registerCourt():
-    return render_template("registerCourt.html", user=current_user)
 
 @views.route('/checkout', methods=['GET'])
 @login_required
